3e702d300b9cd625d8abf544139959856406c7d7
Harold Serrano <harold.serrano@gmail.com>
2016-03-07 20:19:25 -0700

    Updated changes
    Updated files for Vector class

---
+++ Revolver4DMathEngine/Revolver4DMathEngine/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  Revolver4DMathEngine
+//
+//  Created by Harold Serrano on 3/3/16.
+//  Copyright © 2016 haroldserrano.com. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+#define DegreesToRad(angle) angle*M_PI/180
+#define RadToDegrees(angle) angle*180/M_PI
+
+#endif /* Constants_h */
